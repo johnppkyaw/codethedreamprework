3ae3d03fbb4bf0508f2082b142bb973a17f134e7
John Kyaw <jjpyae@gmail.com>
2024-08-11 11:01:36 -0400

    complete same logic in comics.js. edit comics.html, and readme.md

--- comics.js
+++ comics.js
@@ -6,12 +6,31 @@ document.addEventListener('DOMContentLoaded', () => {
 
   //creates hash using timestamp, privatekey, and publickey
   const hash = CryptoJS.MD5(data).toString();
-  console.log(hash);
 
   fetch(`https://gateway.marvel.com/v1/public/comics?limit=20&ts=${ts}&apikey=${myAPIKeys.public}&hash=${hash}`)
     .then(data => data.json())
     .then(result =>  {
-      console.log(result.data.results);
+      let totalChar;
+      let currOffset;
+      let startsWith;
+      let currLetter;
+
+      //After loading the characters, add the buttons
+      const lettersDiv = document.getElementById('letters');
+      const navList = ['Prev', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'Next'];
+      for (const letter of navList) {
+        const eachLetter = document.createElement('div');
+        eachLetter.setAttribute('class', 'each-letter');
+        eachLetter.innerText = letter;
+        eachLetter.onclick = (e) => {
+          startsWith = e.target.innerText.toLowerCase();
+          renderList(startsWith);
+        }
+        lettersDiv.appendChild(eachLetter);
+      }
+                    
+      totalChar = result.data.total;
+      currOffset = result.data.offset;
       result.data.results.forEach(comic => {
         const comicContainer = document.createElement('div');
         comicContainer.setAttribute('class', 'each-comic');
@@ -29,5 +48,76 @@ document.addEventListener('DOMContentLoaded', () => {
 
         comics.appendChild(comicContainer);
       })
+      function renderList(buttonClicked) {
+        let urlString;
+        if(!currLetter) {
+          urlString = `https://gateway.marvel.com/v1/public/comics?orderBy=name&limit=20&ts=${ts}&apikey=${myAPIKeys.public}&hash=${hash}`;
+        } else {
+          urlString = `https://gateway.marvel.com/v1/public/comics?orderBy=name&nameStartsWith=${currLetter}&limit=20&ts=${ts}&apikey=${myAPIKeys.public}&hash=${hash}`;
+        }
+
+        //previous button
+        if(buttonClicked === 'prev') {
+          //if currOffset - 10 >= 0
+          if(currOffset - 20 >= 0) {
+            //update currOffset and add to urlString
+            currOffset = currOffset - 20;
+            urlString += `&offset=${currOffset}`;
+            //fetch and reload result
+            fetchAndReload(urlString);
+          }
+          return;
+        }
+
+        //next button
+        if(buttonClicked === 'next') {
+          //if currOffset + 20 < totalChar
+          if(currOffset + 20 < totalChar) {
+            //update currOffset and add to urlString
+            currOffset = currOffset + 20;
+            urlString += `&offset=${currOffset}`;
+            //fetch and reload result
+            fetchAndReload(urlString);
+          }
+          return;
+        }
+        //neither next or prev
+          currLetter = buttonClicked;
+          //update currOffset to 0;
+          currOffset = 0;
+          //update urlString
+          urlString = `https://gateway.marvel.com/v1/public/comics?orderBy=name&nameStartsWith=${currLetter}&limit=20&ts=${ts}&apikey=${myAPIKeys.public}&hash=${hash}`;
+          //fetch and reload
+          fetchAndReload(urlString);
+          return;
+      }
+
+      function fetchAndReload(url) {
+        comics.innerHTML = "";
+        fetch(url)
+          .then(data => data.json())
+          .then(result => {
+            //update total number of data with the result.data.total;
+            totalChar = result.data.total;
+            result.data.results.forEach(comic => {
+              const comicContainer = document.createElement('div');
+              comicContainer.setAttribute('class', 'each-comic');
+
+              const nameDiv = document.createElement('div');
+              nameDiv.setAttribute('class', 'comic-name');
+              nameDiv.innerText = comic.name;
+
+              const picDiv = document.createElement('div');
+              picDiv.setAttribute('class', 'each-picture');
+              picDiv.style.backgroundImage = `url(${comic.thumbnail.path}.${comic.thumbnail.extension})`
+
+              comicContainer.appendChild(nameDiv);
+              comicContainer.appendChild(picDiv);
+
+              comics.appendChild(charContainer);
+            })
+          })
+        return;
+      }
     })
 })
--- readme.md
+++ readme.md
@@ -1,13 +1,14 @@
-#### Download/Clone the repo
+## Instructions
+### Download/Clone the repo
 
-#### cd into the project folder
+### cd into the project folder
 
-#### npm install inside the folder to install CryptoJS
+### npm install inside the folder to install CryptoJS
 
-#### Get your Marvel public and private api key.
-## By Signing up an account at https://developer.marvel.com/
+### Get your Marvel public and private api key.
+#### By Signing up an account at https://developer.marvel.com/
 
-#### Create a js file with the name myAPIKeys in the same directory as index.js and add the following code:
+### Create a js file with the name myAPIKeys in the same directory as index.js and add the following code:
 
 ``` 
 const myAPIKeys = {
@@ -19,6 +20,6 @@ const myAPIKeys = {
 
 export default myAPIKeys;
 ```
-#### Open index.html to access the project
+### Open index.html to access the project
 
-#### Note: It takes about a minute to get the response from the fetch api calls with Marvel API.
+### Note: It takes about a minute to get the response from the fetch api calls with Marvel API.
